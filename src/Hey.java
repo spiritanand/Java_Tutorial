d373de33f2340ed721047da276b69042ffc9147b
Suryansh Anand <suryansh.anand007@gmail.com>
2020-05-06 22:40:55 +0530

    Added Hey class to the folder

---
+++ src/Hey.java
@@ -0,0 +1,15 @@
+public class Hey
+{
+    /* public is an access modifier. An access modifier allows us to define the scope
+    * public gives us full access. */
+
+    /* Method is a collection of statements that performs an operation. Here we will be using a special
+    method called main. Java looks for this main method always when running a program. It is the entry point of the
+    code. We can also create our own methods.
+     */
+    public static void main(String args[])
+    {
+        // void is used to show that the method will not be returning anything.
+        System.out.println("Hello Java. This is our Universe!");
+    }
+}
