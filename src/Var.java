61889a42801734112195c77422abaaa9e3109940
Suryansh Anand <suryansh.anand007@gmail.com>
2020-05-20 08:20:56 +0530

    Added Variable file

---
+++ src/Var.java
@@ -0,0 +1,11 @@
+public class Var {
+    public static void main (String[] args)
+    {
+        /*
+        Variables are a way to store information. Variables that we define in a program can be accessed by a name
+        we give. The contents of a variable are variable. We need to tell the computer what type of info. we want
+        to store and the name we want to give to it. There are a lot of different data types. Data types are a
+        keyword in Java.
+         */
+    }
+}
