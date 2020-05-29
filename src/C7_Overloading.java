e2bb8249e6510954594381e8d27f5b9a286d3f24
Suryansh Anand <suryansh.anand007@gmail.com>
2020-05-29 10:02:16 +0530

    Added some more coding exercises and added method overloading section

---
+++ src/C7_Overloading.java
@@ -0,0 +1,55 @@
+public class C7_Overloading {
+    public static void main(String args[]) {
+        /*
+        Java can distinguish the methods with different method signatures. i.e. the methods can have same name
+        but with different parameters list (i.e. number of the parameters, order of the parameters, and data
+        types of the parameters). This is very commonly used all around in Java programming.
+
+        Q> Why do we need overloading ?
+        Answer --> If we need to do same kind of the operation with different ways i.e. for different inputs.
+         In the example described below, we are doing length conversion for different inputs (though this
+         example is not that realistic.)
+         It is hard to find many different meaningful names for single action.
+
+        Q> Different ways of method overloading ?
+        Answer --> Method overloading can be done by changing:
+        1.The number of parameters in two methods.
+        2.The data types of the parameters of methods.
+        3.The Order of the parameters of methods.
+
+        Here we are creating to methods of the same name, but of different method signatures our program is
+        a converter of length.
+        In the first method we have two parameters of the double data type and in the second method we have a
+        single parameter of double data type.
+
+
+        NOTE :- Method overloading cannot be done by changing the return type. The method signature needs to be
+         changed for this to work.
+         */
+        calcFeetAndInchesToCm(100);
+    }
+
+    public static double calcFeetAndInchesToCm(double feet, double inches) {
+        if ((feet >= 0) && ((inches >= 0) && (inches <= 12))) {
+            double cm = 12 * (2.54) * feet;
+            cm += 2.54 * inches;
+            System.out.println("The converted length is " + cm + " cm");
+            return (cm);
+        } else {
+            return -1;
+        }
+    }
+
+    public static double calcFeetAndInchesToCm(double inches) {
+        if (inches >= 0) {
+            double feet = (int) inches / 12;
+            double rem_inches = (int) inches % 12;
+            System.out.println(inches + " Inches is equal to " + feet + " feet and " + rem_inches + " inches");
+            return calcFeetAndInchesToCm(feet, rem_inches);
+
+        } else {
+            System.out.println("Invalid Input!");
+            return -1;
+        }
+    }
+}
--- src/CodingExercises1.java
+++ src/CodingExercises1.java
@@ -1,10 +1,10 @@
-public class CodingExercises {
+public class CodingExercises1 {
     public static void main(String args[]){
         /*
-        This section will contain the different the coding exercise of the course that I am following. Here we
+        This section will contain a few different coding exercise of the course that I am following. Here we
         will test our different methods.
          */
-
+        // System.out.println(areEqualByThreeDecimalPlaces(3.176,3.175)); // --> Decimal Comparator check
 
     }
     // This will contain all the methods required for all the coding exercises
@@ -45,23 +45,42 @@ public class CodingExercises {
 //        return false;
 //    }
 
-    // 4.Leap Year Calculator
-    public static boolean isLeapYear(int year){
-        if ((year<1) || (year>9999)){
-            return false;
-        }else {
-            if (year%4==0){
-                if (year%100==0){
-                    if (year%400==0){
-                        return true;
-                    }
-                }else {
-                    return true;
-                }
-            }else{
-                return false;
-            }
-        }
-        return false;
-    }
+
+//    // 4.Leap Year Calculator
+//    public static boolean isLeapYear(int year){
+//        if ((year<1) || (year>9999)){
+//            return false;
+//        }else {
+//            if (year%4==0){
+//                if (year%100==0){
+//                    if (year%400==0){
+//                        return true;
+//                    }
+//                }else {
+//                    return true;
+//                }
+//            }else{
+//                return false;
+//            }
+//        }
+//        return false;
+//    }
+
+
+//    // 5. Decimal Comparator
+//    public static boolean areEqualByThreeDecimalPlaces(double x, double y){
+//        int x1 = (int)(x*1000);
+//        int y1 = (int)(y*1000);
+//        if (x1==y1){
+//            return true;
+//        }
+//        return false;
+//    }
+
+
+    // 6.Equal Sum Checker
+
+
+    // 7.Has Teen Value
+
 }
